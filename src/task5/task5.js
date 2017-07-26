126c6f04dd96fe026342f11fe7c07ab3e6d11d0f
Vitaly Zorin <zorin@competentum.ru>
2017-07-26 19:03:07 +0400

    add loading of new images

--- src/task5/task5.js
+++ src/task5/task5.js
@@ -9,47 +9,69 @@ import 'bootstrap/dist/js/bootstrap';
 import './style.css';
 
 
+const ENTER_KEYCODE = 13;
+const scrollDelay = 1000;
+const reachablePartOfPage = 0.8;
+const IMAGES_PER_QUERY = 10;
+
 let $searchButton = $('#search-button');
 let $inputQuery = $('#search-query');
 let $results = $('#results');
 
+let date = 0;
+let imageIndex = 1; // We can recieve only less than ten images at once. We will change this value every new query
 
-$searchButton.on('click', function() {
+
+$searchButton.on('click', function () {
+  eraseResults();
   search();
-});
+})
 
-$inputQuery.on('keypress', function(event) {
-  if (event.keyCode === 13) {
+$inputQuery.on('keypress', function (event) {
+  if (event.keyCode === ENTER_KEYCODE) {
     event.preventDefault();
+    eraseResults();
     search();
   }
 });
 
-window.addEventListener('scroll', function() {
-  if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight*0.8) {
+window.addEventListener('scroll', function () { //load new images when user reaches bottom part of the page
+
+  if (new Date() - date < scrollDelay) return;  //delay is required to prevent excess call of search
+
+  date = new Date();
+
+  if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight * reachablePartOfPage) {
     search();
   }
+
 });
 
 
 function search() {
   let googleApiLink = createQuerry($inputQuery.val());
   executeQuerry(googleApiLink);
+
+  // if (window.innerHeight >= document.body.offsetHeight * reachablePartOfPage) { //fill page with images until scroll appear
+  //   setTimeout(search, 1000);
+  // }
 }
 
-function createQuerry (queryString) {
+function createQuerry(queryString) {
   const engineID = '004486783337092485371%3Axcbayn685lg';
-  const apiKey = 'AIzaSyDNAa8dTm1yy8TRKepf7TjwyrOceTfbnxk';
+  const apiKey = 'AIzaSyDPCFJ7Fv3-QxxXzcAfIywKEPLWqclxE4I';
 
-  return `https://www.googleapis.com/customsearch/v1?q=${queryString}&cx=${engineID}&safe=high&searchType=image&key=${apiKey}`;
+  let googleQuery = `https://www.googleapis.com/customsearch/v1?q=${queryString}&cx=${engineID}&start=${imageIndex}&safe=high&searchType=image&key=${apiKey}`;
+  imageIndex += IMAGES_PER_QUERY;
+  console.log(googleQuery);;
+  return googleQuery;
 }
 
 function executeQuerry(query) {
   $.getJSON(query)
-    .done(function( data ) {
-      console.dir(data);
+    .done(function (data) {
 
-      $.each( data.items, function( i, item ) {
+      $.each(data.items, function (i, item) {
         let $bootstrapContainer = $('<div class="col-xs-6 col-md-3"></div>');
         let $link = $('<a>').attr(
           {
@@ -58,11 +80,16 @@ function executeQuerry(query) {
             'class': 'thumbnail'
           });
 
-        let $image = $( '<img>' ).attr("src", item.link);
+        let $image = $('<img>').attr("src", item.link);
 
         $link.append($image);
         $bootstrapContainer.append($link).appendTo($results);
 
       });
     });
+}
+
+function eraseResults() {
+  imageIndex = 1;
+  $results.html('');
 }
